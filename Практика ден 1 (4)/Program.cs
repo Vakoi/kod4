339d5dc1df09132cfb7f7dd0d218276376cea074
Vakoi <mak-uk@mail.ru>
2023-04-17 16:09:10 +0300

    Добавьте файлы проекта.

---
+++ Практика ден 1 (4)/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int N = 120; // замените на нужное значение
+
+        int minutes = (N / 60) % 60;
+
+        Console.WriteLine("Количество полных минут с начала последнего часа: " + minutes);
+    }
+}
